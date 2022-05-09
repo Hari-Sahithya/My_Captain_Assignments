807966aa25cf1db15ac271c4282378f72008bb6c
Hari-Sahithya <105123023+Hari-Sahithya@users.noreply.github.com>
2022-05-09 12:18:55 +0530

    Add files via upload

---
+++ areaofcircle.py
@@ -0,0 +1,17 @@
+def area(circle):
+    return area(pi*(r*r))
+pi=3.14
+r=2
+circle=pi*r*r
+print(circle)
+
+
+
+
+
+
+def extention(file):
+    return extention("filename.split")
+filename="sahithya.py"
+file=filename.split(".")[-1]
+print(file)
